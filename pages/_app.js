54978f249ddcaa676292a8217620bb3b7bb12e00
SpookyBrenda <82229644+SpookyBrenda@users.noreply.github.com>
2022-11-04 15:19:06 +0000

    Add global style component to prevent overscroll

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import { Global, css } from '@emotion/react';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -20,6 +21,11 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <Global styles={css`
+          html { 
+            overscroll-behavior: none;
+          }
+        `}/>
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
